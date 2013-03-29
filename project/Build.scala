ae009c9abd050c6204bda50ed04fca444178058e
Michael Schmitz <michael@schmitztech.com>
2013-03-28 18:28:24 -0700

    Upgrade to 2.10.

--- project/Build.scala
+++ project/Build.scala
@@ -7,20 +7,20 @@ object ApplicationBuild extends Build {
     val appName         = "documentextractor"
     val appVersion      = "1.0-SNAPSHOT"
 
-    val nlptoolsVersion = "2.4.0"
+    val nlptoolsVersion = "2.4.1-SNAPSHOT"
     val nlptoolsGroupId = "edu.washington.cs.knowitall.nlptools"
 
     val appDependencies = Seq(
-      "edu.washington.cs.knowitall.ollie" % "ollie-core_2.9.2" % "1.0.2",
-      "edu.washington.cs.knowitall.chunkedextractor" % "chunkedextractor_2.9.2" % "1.0.2",
+      "edu.washington.cs.knowitall.ollie" %% "ollie-core" % "1.0.3-SNAPSHOT",
+      "edu.washington.cs.knowitall.chunkedextractor" %% "chunkedextractor" % "1.0.3-SNAPSHOT",
       "org.apache.tika" % "tika-app" % "1.2",
-      "org.scalaz" % "scalaz_2.9.2" % "6.0.4",
-      nlptoolsGroupId % "nlptools-parse-malt_2.9.2" % nlptoolsVersion,
-      nlptoolsGroupId % "nlptools-chunk-opennlp_2.9.2" % nlptoolsVersion,
-      nlptoolsGroupId % "nlptools-sentence-opennlp_2.9.2" % nlptoolsVersion,
-      nlptoolsGroupId % "nlptools-coref-stanford_2.9.2" % nlptoolsVersion,
-      "edu.washington.cs.knowitall.openiesrl" % "openie-srl_2.9.2" % "1.0-SNAPSHOT",
-      "net.databinder.dispatch" % "dispatch-core_2.9.2" % "0.9.5",
+      "org.scalaz" %% "scalaz" % "6.0.4",
+      nlptoolsGroupId %% "nlptools-parse-malt" % nlptoolsVersion,
+      nlptoolsGroupId %% "nlptools-chunk-opennlp" % nlptoolsVersion,
+      nlptoolsGroupId %% "nlptools-sentence-opennlp" % nlptoolsVersion,
+      nlptoolsGroupId %% "nlptools-coref-stanford" % nlptoolsVersion,
+      "edu.washington.cs.knowitall.openiesrl" %% "openie-srl" % "1.0-SNAPSHOT",
+      "net.databinder.dispatch" %% "dispatch-core" % "0.9.5",
       "de.l3s.boilerpipe" % "boilerpipe" % "1.2.0",
       "net.sourceforge.nekohtml" % "nekohtml" % "1.9.18",
       "xerces" % "xercesImpl" % "2.9.1",
@@ -29,7 +29,7 @@ object ApplicationBuild extends Build {
       "org.apache.commons" % "commons-lang3" % "3.1"
     )
 
-    val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
+    val main = play.Project(appName, appVersion, appDependencies).settings(
       // Add your own project settings here
       resolvers += "boilerpipe-m2-repo" at "https://boilerpipe.googlecode.com/svn/repo/",
       resolvers += "sonatype-snapshot" at "https://oss.sonatype.org/content/repositories/snapshots/"
